0407d716664f4de4b90b217ea7818a0ad91fba2f
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-07-16 15:20:47 +0200

    added schema listing

---
+++ src/Listing/ListSchema.php
@@ -0,0 +1,73 @@
+<?php
+namespace KiwiSuite\Schema\Listing;
+
+use KiwiSuite\Contract\Schema\Listing\ElementInterface;
+use KiwiSuite\Contract\Schema\Listing\ListSchemaInterface;
+
+final class ListSchema implements ListSchemaInterface
+{
+    /**
+     * @var ElementInterface[]
+     */
+    private $elements = [];
+
+    /**
+     * @var null|array
+     */
+    private $defaultSorting = null;
+
+    /**
+     * @return ElementInterface[]
+     */
+    public function elements(): array
+    {
+        return $this->elements;
+    }
+
+    /**
+     * @param ElementInterface $element
+     * @return ListSchemaInterface
+     */
+    public function withAddedElement(ElementInterface $element): ListSchemaInterface
+    {
+        $schema = clone $this;
+        $schema->elements[$element->name()] = $element;
+
+        return $schema;
+    }
+
+    /**
+     * @return array
+     */
+    public function defaultSorting(): ?array
+    {
+        return $this->defaultSorting;
+    }
+
+    /**
+     * @param string $defaultSorting
+     * @param string $direction
+     * @return ListSchemaInterface
+     */
+    public function withDefaultSorting(string $defaultSorting, string $direction): ListSchemaInterface
+    {
+        $schema = clone $this;
+        $schema->defaultSorting = [
+            'sorting' => $defaultSorting,
+            'direction' => $direction,
+        ];
+
+        return $schema;
+    }
+
+    /**
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'elements' => array_values($this->elements),
+            'defaultSorting' => $this->defaultSorting,
+        ];
+    }
+}
