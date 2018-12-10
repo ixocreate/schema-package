0855d11b89dbe928245a82f6f341819a644c3aa1
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-12-10 14:52:42 +0100

    added AdditionalSchemaSubManager

--- bootstrap/servicemanager.php
+++ bootstrap/servicemanager.php
@@ -3,8 +3,11 @@ declare(strict_types=1);
 
 namespace KiwiSuite\Schema;
 
+use KiwiSuite\Schema\AdditionalSchema\AdditionalSchemaSubManager;
+
 /** @var \KiwiSuite\ServiceManager\ServiceManagerConfigurator $serviceManager */
 
 $serviceManager->addSubManager(ElementSubManager::class);
+$serviceManager->addSubManager(AdditionalSchemaSubManager::class);
 
 $serviceManager->addFactory(Builder::class);
