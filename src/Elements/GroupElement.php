23b8665e1727b56420161bcb06c9dcfe172d63c7
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-12-21 23:14:58 +0100

    added icon support

--- src/Elements/GroupElement.php
+++ src/Elements/GroupElement.php
@@ -16,18 +16,63 @@ use KiwiSuite\Contract\Schema\StructuralGroupingInterface;
 
 final class GroupElement extends AbstractGroup implements StructuralGroupingInterface
 {
+    /**
+     * @var string|null
+     */
+    private $icon;
+
+    /**
+     * @return string
+     */
     public function inputType(): string
     {
         return SchemaType::class;
     }
 
+    /**
+     * @return string
+     */
     public function type(): string
     {
         return 'group';
     }
 
+    /**
+     * @return string
+     */
     public static function serviceName(): string
     {
         return 'group';
+    }
+
+    /**
+     * @param string $icon
+     * @return GroupElement
+     */
+    public function withIcon(string $icon): GroupElement
+    {
+        $group = clone $this;
+        $group->icon = $icon;
+
+        return $group;
+    }
+
+    /**
+     * @return string
+     */
+    public function icon(): ?string
+    {
+        return $this->icon;
+    }
+
+    /**
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        $array = parent::jsonSerialize();
+        $array['icon'] = $this->icon();
+
+        return $array;
     }
 }
