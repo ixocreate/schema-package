2c95e6238580f8e3555d61938ca84845aacef0b8
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2018-12-27 14:39:46 +0100

    phpunit setup

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Test\Template;
+
+\chdir(\dirname(__DIR__));
+include 'vendor/autoload.php';
