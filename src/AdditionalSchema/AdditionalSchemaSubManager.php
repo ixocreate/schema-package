576f6601ae4a099170ee8f97531ff1bcd6b9117b
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2019-01-29 15:15:33 +0100

    added SchemaProviderInterface

--- src/AdditionalSchema/AdditionalSchemaSubManager.php
+++ src/AdditionalSchema/AdditionalSchemaSubManager.php
@@ -11,16 +11,17 @@ namespace Ixocreate\Schema\AdditionalSchema;
 
 use Ixocreate\Contract\Schema\AdditionalSchemaInterface;
 use Ixocreate\Contract\Schema\SchemaInterface;
-use Ixocreate\Contract\Schema\SchemaReceiverInterface;
+use Ixocreate\Contract\Schema\SchemaProviderInterface;
 use Ixocreate\Schema\Builder;
 use Ixocreate\ServiceManager\SubManager\SubManager;
 
-class AdditionalSchemaSubManager extends SubManager implements SchemaReceiverInterface
+class AdditionalSchemaSubManager extends SubManager implements SchemaProviderInterface
 {
-    public function receiveSchema(Builder $builder, array $options = []): SchemaInterface
+
+    public function provideSchema($name, Builder $builder, $options = []): SchemaInterface
     {
         /** @var AdditionalSchemaInterface $additionalSchema */
-        $additionalSchema = $this->get($options['additionalSchema']);
+        $additionalSchema = $this->get($name);
 
         return $additionalSchema->additionalSchema($builder);
     }
