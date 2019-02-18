1651a74d0f597ac61192c55a8da2d57eb83dd916
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2019-02-18 15:00:20 +0100

    fix: contracts

--- src/AdditionalSchema/AdditionalSchemaSubManager.php
+++ src/AdditionalSchema/AdditionalSchemaSubManager.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace Ixocreate\Schema\AdditionalSchema;
 
 use Ixocreate\Contract\Schema\AdditionalSchemaInterface;
+use Ixocreate\Contract\Schema\BuilderInterface;
 use Ixocreate\Contract\Schema\SchemaInterface;
 use Ixocreate\Contract\Schema\SchemaProviderInterface;
 use Ixocreate\Schema\Builder;
@@ -18,7 +19,7 @@ use Ixocreate\ServiceManager\SubManager\SubManager;
 class AdditionalSchemaSubManager extends SubManager implements SchemaProviderInterface
 {
 
-    public function provideSchema($name, Builder $builder, $options = []): SchemaInterface
+    public function provideSchema($name, BuilderInterface $builder, $options = []): SchemaInterface
     {
         /** @var AdditionalSchemaInterface $additionalSchema */
         $additionalSchema = $this->get($name);
