10165e406f4c997b338635ff869040f5af43d4ec
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2019-05-27 12:32:41 +0200

    deprecated fromEntity method

---
+++ tests/misc/FakeEntity.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+namespace Ixocreate\Misc\Schema;
+
+use Ixocreate\Schema\Builder\BuilderInterface;
+use Ixocreate\Schema\Element\TextElement;
+use Ixocreate\Schema\Schema;
+use Ixocreate\Schema\SchemaAwareInterface;
+use Ixocreate\Schema\SchemaInterface;
+
+class FakeEntity implements SchemaAwareInterface
+{
+    private function __construct()
+    {
+
+    }
+
+    public function schema(BuilderInterface $builder): SchemaInterface
+    {
+        return (new Schema())
+            ->withAddedElement($builder->create(TextElement::serviceName(), 'test'));
+    }
+}
---
+++ tests/src/Builder/BuilderTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Ixocreate\Test\Schema\Builder;
+
+use Ixocreate\Misc\Schema\FakeEntity;
+use Ixocreate\Schema\Builder\Builder;
+use Ixocreate\Schema\Element\ElementSubManager;
+use Ixocreate\Schema\Element\TextElement;
+use Ixocreate\Schema\Schema;
+use PHPUnit\Framework\TestCase;
+
+class BuilderTest extends TestCase
+{
+    public function testFromEntity()
+    {
+        $elementSubManager = $this->createMock(ElementSubManager::class);
+        $elementSubManager->method('get')->willReturnCallback(function () {
+            return new TextElement();
+        });
+
+        $builder = new Builder($elementSubManager);
+
+        $schema = $builder->fromEntity(FakeEntity::class);
+        $this->assertInstanceOf(Schema::class, $schema);
+        $this->assertTrue($schema->has('test'));
+    }
+}
