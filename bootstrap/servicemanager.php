3ccdf14b2c68deb73f5c94545092741a3d0b4f4f
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2019-05-29 16:57:11 +0200

    add BuilderFactory

--- bootstrap/servicemanager.php
+++ bootstrap/servicemanager.php
@@ -11,6 +11,8 @@ namespace Ixocreate\Schema;
 
 use Ixocreate\Application\Service\ServiceManagerConfigurator;
 use Ixocreate\Schema\Builder\Builder;
+use Ixocreate\Schema\Builder\BuilderFactory;
+use Ixocreate\Schema\Builder\BuilderInterface;
 use Ixocreate\Schema\Element\ElementSubManager;
 use Ixocreate\Schema\Link\LinkManager;
 use Ixocreate\Schema\Type\TypeSubManager;
@@ -21,4 +23,4 @@ $serviceManager->addSubManager(SchemaSubManager::class);
 $serviceManager->addSubManager(TypeSubManager::class);
 $serviceManager->addSubManager(LinkManager::class);
 
-$serviceManager->addFactory(Builder::class);
+$serviceManager->addFactory(BuilderInterface::class, BuilderFactory::class);
---
+++ src/Builder/BuilderFactory.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOLIT GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Schema\Builder;
+
+use Ixocreate\Schema\Element\ElementSubManager;
+use Ixocreate\ServiceManager\FactoryInterface;
+use Ixocreate\ServiceManager\ServiceManagerInterface;
+
+class BuilderFactory implements FactoryInterface
+{
+    /**
+     * @param ServiceManagerInterface $container
+     * @param $requestedName
+     * @param array|null $options
+     * @return Builder|mixed
+     */
+    public function __invoke(ServiceManagerInterface $container, $requestedName, array $options = null)
+    {
+        return new Builder($container->get(ElementSubManager::class));
+    }
+}
