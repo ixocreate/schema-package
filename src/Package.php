e47d996b5d55a338f9f8ef170ed3394eabd427e8
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2019-06-12 09:52:35 +0200

    update for application 0.3

--- src/Package.php
+++ src/Package.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace Ixocreate\Schema;
 
 use Ixocreate\Application\Configurator\ConfiguratorRegistryInterface;
+use Ixocreate\Application\Package\BootInterface;
 use Ixocreate\Application\Package\PackageInterface;
 use Ixocreate\Application\Service\ServiceRegistryInterface;
 use Ixocreate\Schema\Element\ElementBootstrapItem;
@@ -19,22 +20,8 @@ use Ixocreate\Schema\Type\TypeBootstrapItem;
 use Ixocreate\Schema\Type\TypeSubManager;
 use Ixocreate\ServiceManager\ServiceManagerInterface;
 
-final class Package implements PackageInterface
+final class Package implements PackageInterface, BootInterface
 {
-    /**
-     * @param ConfiguratorRegistryInterface $configuratorRegistry
-     */
-    public function configure(ConfiguratorRegistryInterface $configuratorRegistry): void
-    {
-    }
-
-    /**
-     * @param ServiceRegistryInterface $serviceRegistry
-     */
-    public function addServices(ServiceRegistryInterface $serviceRegistry): void
-    {
-    }
-
     /**
      * @return array|null
      */
@@ -45,14 +32,6 @@ final class Package implements PackageInterface
             ElementBootstrapItem::class,
             LinkBootstrapItem::class,
         ];
-    }
-
-    /**
-     * @return array|null
-     */
-    public function getConfigProvider(): ?array
-    {
-        return null;
     }
 
     /**
@@ -73,14 +52,6 @@ final class Package implements PackageInterface
     public function getBootstrapDirectory(): ?string
     {
         return __DIR__ . '/../bootstrap';
-    }
-
-    /**
-     * @return null|string
-     */
-    public function getConfigDirectory(): ?string
-    {
-        return null;
     }
 
     /**
